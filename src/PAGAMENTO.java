c4b75adccba69bc60fafae866f5acd97e947a71f
Emerson Johnny <emersonjohnny2015@hotmail.com>
2023-10-23 16:34:25 -0300

    First commit

---
+++ src/PAGAMENTO.java
@@ -0,0 +1,12 @@
+
+public class PAGAMENTO {
+    private COBRANCA valorpagto;
+    private COBRANCA datapagto;
+    private COBRANCA juros;
+}
+
+
+
+
+
+//https://online.visual-paradigm.com/w/anfnplte/diagrams/#diagram:proj=0&type=ClassDiagram&width=11&height=8.5&unit=inch
